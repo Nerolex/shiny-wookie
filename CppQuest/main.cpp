2abc020a9d63a8a1c285bdeea03575fba50ed527
Michael Glasder <Michael@Michaels-MacBook-Pro.local>
2015-01-10 15:18:33 +0100

    Initial Commit

---
+++ CppQuest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CppQuest
+//
+//  Created by Michael Glasder on 10.01.15.
+//  Copyright (c) 2015 Michael Glasder. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
